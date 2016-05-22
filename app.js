611211d9b2972b778d16946dd3920c8d3483acc4
enoch232 <enoch232@gmail.com>
2016-05-22 20:48:15 +0900

    add book.js model, and add mongoose oose information

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var app = express();
+mongoose.connect('mongodb://localhost/books');
+var db = mongoose.connection;
+app.get("/",function(req, res){
+	res.end("Hello World");
+});
+app.listen(3000);
+console.log("Server is now running at port 3000..")
